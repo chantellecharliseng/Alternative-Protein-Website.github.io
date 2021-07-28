a388cbe0159682a3f342aacbe4045fd225a72d7f
Ng <doratheexplorerlovesdeigo@gmail.com>
2021-07-28 13:05:26 -0400

    added cname

---
+++ main.js
@@ -0,0 +1,19 @@
+// create two variables to access the character and block 
+
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+
+/* the leap function
+- access the character's class list and add "animate"
+- animate is what we described in css
+- this function is initialized by the "onlick" at the top of the game.html file 
+*/
+function leap(){
+   if(character.classList == "animate"){return}
+   character.classList.add("animate");
+   setTimeout(function(){
+   character.classList.remove("animate");
+   },300);
+}
+
+
